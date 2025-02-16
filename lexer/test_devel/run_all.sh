43facc2bfa1cd0404b4881b87ef0db773e5650a6
Piotr Grabowski <piotr@quesma.com>
2025-02-16 04:35:26 +0100

    run-all

---
+++ lexer/test_devel/run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
+sqlparse/extract_testcases/extract_testcases.sh
+sqlparse/parse_testcases/parse_testcases.sh
--- lexer/test_devel/sqlparse/extract_testcases/extract_testcases.sh
+++ lexer/test_devel/sqlparse/extract_testcases/extract_testcases.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 IMAGE="nixos/nix@sha256:3bb728719e2c4e478df4c50b80f93adbe27d5c561d1417c3a2306eb914d910da"
 
 rm -f "$(dirname "$0")"/../../../dialect_sqlparse/test_files/extracted-sqlparse-testcases.txt
 
-docker run -v "$(dirname "$0")":/mount -v "$(dirname "$0")"/../../../dialect_sqlparse/test_files:/output --rm -i "$IMAGE" /bin/sh -c "cd /mount; nix-shell shell.nix --run 'bash run_testcases.sh'"
+docker run -v "$SCRIPTPATH":/mount -v "$SCRIPTPATH"/../../../dialect_sqlparse/test_files:/output --rm -i "$IMAGE" /bin/sh -c "cd /mount; nix-shell shell.nix --run 'bash run_testcases.sh'"
