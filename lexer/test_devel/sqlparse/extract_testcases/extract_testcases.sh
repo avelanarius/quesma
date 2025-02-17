dd0b873d01d8833c70b1dfeb1284a8bb9609f18c
Piotr Grabowski <piotr@quesma.com>
2025-02-17 11:00:58 +0100

    nix caching

--- lexer/test_devel/sqlparse/extract_testcases/extract_testcases.sh
+++ lexer/test_devel/sqlparse/extract_testcases/extract_testcases.sh
@@ -3,4 +3,4 @@
 ROOTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/../../../
 IMAGE="nixos/nix@sha256:3bb728719e2c4e478df4c50b80f93adbe27d5c561d1417c3a2306eb914d910da"
 
-docker run -v "$ROOTPATH":/mount --rm -i "$IMAGE" /bin/sh -c "cd /mount/test_devel/sqlparse/extract_testcases; nix-shell shell.nix --run 'bash run_testcases.sh'"
+docker run -v quesma_lexer_nix:/nix -v quesma_lexer_nix_cache:/root/.cache/ -v "$ROOTPATH":/mount --rm -i "$IMAGE" /bin/sh -c "cd /mount/test_devel/sqlparse/extract_testcases; nix-shell shell.nix --run 'bash run_testcases.sh'"
